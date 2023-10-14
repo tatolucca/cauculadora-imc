f059684fb740c9521a43e63696ac4b21f6ecd2e8
lucca_system <144394390+tatolucca@users.noreply.github.com>
2023-10-14 20:37:37 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,50 +1,42 @@
-// Função para calcular o IMC
-function calcularIMC(peso, altura) {
-    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
-        return "Valores inválidos";
+document.addEventListener('DOMContentLoaded', function () {
+    // Esta função calcula o IMC
+    function calcularIMC() {
+        var peso = parseFloat(document.getElementById("peso").value);
+        var altura = parseFloat(document.getElementById("altura").value);
+
+        if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+            alert("Por favor, insira um peso e altura válidos.");
+            return;
+        }
+
+        // Calcular IMC
+        var imc = peso / (altura * altura);
+
+        // Mostrar o IMC
+        document.getElementById("imc-valor").innerText = imc.toFixed(2);
+
+        // Interpretar o IMC
+        interpretarIMC(imc);
     }
 
-    const imc = peso / (altura * altura);
-    return imc.toFixed(2);
-}
+    // Esta função interpreta o IMC
+    function interpretarIMC(imc) {
+        var resultado = document.getElementById("imc-classificacao");
 
-// Função para lidar com o clique no botão de cálculo
-function calcular() {
-    const pesoInput = document.getElementById('peso');
-    const alturaInput = document.getElementById('altura');
-    const imcValor = document.getElementById('imc-valor');
-    const imcClassificacao = document.getElementById('imc-classificacao');
-
-    const peso = parseFloat(pesoInput.value);
-    const altura = parseFloat(alturaInput.value);
-
-    const resultado = calcularIMC(peso, altura);
-
-    if (resultado === "Valores inválidos") {
-        imcValor.textContent = "Valores inválidos";
-        imcClassificacao.textContent = "";
-    } else {
-        imcValor.textContent = resultado;
-        imcClassificacao.textContent = classificarIMC(parseFloat(resultado));
+        if (imc < 18.5) {
+            resultado.innerText = "Você está abaixo do peso (magro).";
+        } else if (imc < 24.9) {
+            resultado.innerText = "Seu peso está dentro da faixa saudável.";
+        } else if (imc < 29.9) {
+            resultado.innerText = "Você está com sobrepeso.";
+        } else {
+            resultado.innerText = "Você está obeso.";
+        }
     }
-}
 
-// Função para classificar o IMC
-function classificarIMC(imc) {
-    if (imc < 18.5) {
-        return "Abaixo do peso";
-    } else if (imc < 24.9) {
-        return "Peso normal";
-    } else if (imc < 29.9) {
-        return "Sobrepeso";
-    } else if (imc < 34.9) {
-        return "Obesidade Grau 1";
-    } else if (imc < 39.9) {
-        return "Obesidade Grau 2";
-    } else {
-        return "Obesidade Grau 3";
-    }
-}
-
-// Adicione um ouvinte de evento ao botão de cálculo
-document.getElementById('calcular').addEventListener('click', calcular);
+    // Adicionar um evento ao botão de calcular
+    var calcularButton = document.getElementById("calcular");
+    calcularButton.addEventListener("click", function () {
+        calcularIMC();
+    });
+});
